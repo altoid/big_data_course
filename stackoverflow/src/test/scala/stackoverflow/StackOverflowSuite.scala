24859f64e04a7c2043b8356208a343fe5f5c47ae
Doug Tomm <dtomm@radiumone.com>
2018-03-14 08:34:42 -0700

    set up test config and trivial test

--- stackoverflow/src/test/scala/stackoverflow/StackOverflowSuite.scala
+++ stackoverflow/src/test/scala/stackoverflow/StackOverflowSuite.scala
@@ -34,5 +34,22 @@ class StackOverflowSuite extends FunSuite with BeforeAndAfterAll {
     assert(instantiatable, "Can't instantiate a StackOverflow object")
   }
 
+  test("simple") {
+    import StackOverflow._
+    val postings = List(
+      Posting(1, 27233496, None, None, 0, Some("C#")),
+      Posting(1, 23698767, None, None,9, Some("C#")),
+      Posting(1, 5484340, None, None,0, Some("C#")),
+      Posting(2, 5494879, None, Some(5484340),1, None),
+      Posting(1, 9419744, None, None,2, Some("Objective-C")),
+      Posting(1, 26875732, None, None,1, Some("C#")),
+      Posting(1, 9002525, None, None,2, Some("C++")),
+      Posting(2, 9003401, None, Some(9002525),4, None),
+      Posting(2, 9003942, None, Some(9002525),1, None),
+      Posting(2, 9005311, None, Some(9002525),0, None)
+    )
 
+    val rdd = sc.parallelize(postings)
+    rdd.collect().foreach(println)
+  }
 }
